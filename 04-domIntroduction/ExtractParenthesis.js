3bdb86aad3c3986bb32ee5c8935a447f9f56af34
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-01-21 16:13:02 +0200

    100/100

--- 04-domIntroduction/ExtractParenthesis.js
+++ 04-domIntroduction/ExtractParenthesis.js
@@ -1,3 +1,17 @@
-function extract(content) {
+function extract() {
 
+    let content = document.getElementById('content').textContent;
+    let pattern = /\(([^)]+)\)/g;
+    let result = [];
+    let match = pattern.exec(content);
+
+    while (match != null) {
+
+        if (match) {
+            result.push(match[1])
+        }
+        match = pattern.exec(content);
+    }
+
+    return result.join('; ')
 }
